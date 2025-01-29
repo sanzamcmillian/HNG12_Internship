90b9566e51ba15e502274d6ffe47d7dd6870eac7
sanzamcmillian <g18s2658@ru.ac.za>
2025-01-30 01:54:43 +0200

    the first working code

---
+++ stage_0/main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from datetime import datetime
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def get_info():
+    data = {
+        "email": "sanzamcmillian@gmail.com",
+        "current_datetime": datetime.utcnow().replace(microsecond=0).isoformat() + "Z",
+        "github_url": "https://github.com/sanzamcmillian/HNG12_Internship/tree/main/stage_0"
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
