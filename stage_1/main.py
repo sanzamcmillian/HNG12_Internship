9f2937f2036b12ec02be34fbc2e4cf4aa81a6d9a
sanzamcmillian <g18s2658@ru.ac.za>
2025-02-05 14:52:36 +0200

    Update main.py
    optimize code

--- stage_1/main.py
+++ stage_1/main.py
@@ -4,29 +4,36 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 import os
+from functools import lru_cache
 
 app = Flask(__name__)
 CORS(app)
 
-
+@lru_cache(maxsize=1024)
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, int(abs(n) ** 0.5) + 1):
+    if n in (2, 3):
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    for i in range(5, int(abs(n) ** 0.5) + 1, 2):
         if abs(n) % i == 0:
             return False
     return True
 
+@lru_cache(maxsize=1024)
 def is_perfect(n):
     if n < 0:
         return False
-    return sum(i for i in range(1, n) if n % i == 0) == n
+    total  = sum(i for i in range(1, int(n / 2) + 1) if n % i == 0)
+    return total == n
 
-
+@lru_cache(maxsize=1024)
 def is_armstrong(n):
     if n < 0:
         return False
-    digits = [int(d) for d in str(abs(n))]
+    digits = list(map(int, str(abs(n))))
     power = len(digits)
     return sum(d ** power for d in digits) == abs(n)
 
@@ -50,20 +57,12 @@ def classify_number():
         return jsonify({"number": num_str, "error": True}), 400
 
 
-    properties = []
+    properties = ["even" if num % 2 == 0 else "odd"]
 
     if is_armstrong(num):
         properties.append("armstrong")
 
-    if num % 2 == 1:
-        properties.append("odd")
-    else:
-        properties.append("even")
-
     digit_sum = sum(int(d) for d in str(abs(num)))
-
-    if num < 0:
-        digit_sum = -digit_sum
 
     response = {
         "number": num,
@@ -77,5 +76,4 @@ def classify_number():
     return jsonify(response), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0', port=5000, debug=True)
